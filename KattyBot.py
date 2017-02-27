8cd8ef4badb81ebe563124a27b3bff5d1d05460c
bruniii <marbrunero@gmail.com>
2017-02-27 16:02:56 +0100

    Stop e chat_ID
    Aggiunto /stop per disabilitare le risposte, tramite flag_Chat.
    
    Aggiunto /chatID per poter leggere l'ID del gruppo. L'ID di Spam verrà
    poi salvato a mano in SpamID e ogni comando del bot dovrà sfruttare il
    filtro filter_spam in aggiunta al filtro specifico dell'handler. Per ora
    è stato implementato solo in batman_handler.

--- KattyBot.py
+++ KattyBot.py
@@ -1,9 +1,11 @@
-# Prima prova del Bot in Telegram.
+"""" Prima prova del Bot in Telegram.
+
 # seguita la guida in:
 # https://github.com/python-telegram-bot/python-telegram-bot
 # Nome del Bot: Sopresa
 # Username: @KattyBot
-# Token: 362591666:AAEwquW77vwbnwDhK2899SGUoW4emmKoLQk
+# Token: 362591666:AAEwquW77vwbnwDhK2899SGUoW4emmKoLQk.
+"""
 
 import logging
 from telegram.ext import Updater
@@ -11,58 +13,86 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import BaseFilter
 
+flag_Chat = True
+IDchat = 0
+SpamID = 163078549  # Marco: 163078549
+
 updater = Updater(token='362591666:AAEwquW77vwbnwDhK2899SGUoW4emmKoLQk')
 
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Teo SUKA!")
+    """Funzione chiamata da /start@KattyBot. Penso sia necessario chiamarla una volta. Oltre a scrivere un saluto, imposta anche il flag su True e questo viene usato per abilitare il resto delle conversazioni."""
+    global flag_Chat
+    bot.sendMessage(chat_id=update.message.chat_id, text="Ciao a tutti! KAFFFEEH?")
+    flag_Chat = True
+
+
+def stop(bot, update):
+    """"Funzione chiamata da /stop@KattyBot. Oltre a scrivere un addio, imposta anche il flag su False."""
+    global flag_Chat
+    bot.sendMessage(chat_id=update.message.chat_id, text="Me ne vado. Ma prima voglio dire una cosa: A Beppe piace la Kattyyyy")
+    flag_Chat = False
 
 
 start_handler = CommandHandler('start', start)
+stop_handler = CommandHandler('stop', stop)
 dispatcher.add_handler(start_handler)
-# Per chiamare questo comando è sufficiente nella chat di
-# Telegram scrivere /start@KattyBot
+dispatcher.add_handler(stop_handler)
 
-
-def echo(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='Ogni volta che scrivi qualcosa io rispondo così.')
-
-
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-# dispatcher.remove_handler(echo_handler)
 
 def batman(bot, update):
-    bot.sendPhoto(chat_id=update.message.chat_id, photo='https://selfie.legobatman.com/assets/img/BatmanLegalScreen.png')
+    """La funzione callback che viene chiamata dalla parola chiave batman."""
+    # TODO: scoprire come usare la caption del comando sendPhoto
+    if flag_Chat is True:
+        bot.sendPhoto(chat_id=update.message.chat_id, photo='https://selfie.legobatman.com/assets/img/BatmanLegalScreen.png', caption='NANANANA')
 
 
 class FilterBatman(BaseFilter):
+    """Questa parte si occupa di verificare la presenza della parola chiave "batman" nel testo del messaggio. Se è presente, ritorna 1 e quindi viene chiamata la funzione di callback batman."""
     def filter(self, message):
         return 'batman' in message.text
 
 
-filter_batman = FilterBatman()
-
-batman_handler = MessageHandler(filter_batman, batman)
-dispatcher.add_handler(batman_handler)
-
-# Questo è handler vuoto che uso solo per capire l'ID della chat,
-# quando qualcuno scrive il comando /chat_id
-def chat_id(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.chat_id)
-
-class FilterChatID(BaseFilter):
+class FilterSpam(BaseFilter):
+    """Questa parte si occupa di verificare la presenza della parola chiave "batman" nel testo del messaggio. Se è presente, ritorna 1 e quindi viene chiamata la funzione di callback batman."""
     def filter(self, message):
-        return 'chat_id' in message.text
+        """Controlla che l'ID della chat sia uguale a quello dello Spam."""
+        return SpamID == message.chat_id
 
 
-filter_chat = FilterChatID()
+filter_batman = FilterBatman()
+filter_spam = FilterSpam()
 
-chatID_handler = CommandHandler(filter_chat, chat_id)
+batman_handler = MessageHandler(filter_batman & filter_spam, batman)
+dispatcher.add_handler(batman_handler, group=0)
+
+
+def echo(bot, update):
+    """Semplice comando: ogni volta che sniffa un messaggio, risponde con una frase."""
+    if flag_Chat is True:
+        bot.sendMessage(chat_id=update.message.chat_id, text='Ogni volta che scrivi qualcosa io rispondo così.')
+
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler, group=0)
+
+def chatID(bot, update):
+    """"Questo è handler vuoto che uso solo per capire l'ID della chat,
+    quando qualcuno scrive il comando /chat_id"""
+    global IDchat
+    user = update.message.from_user
+    IDchat = update.message.chat.id
+    print('Stai parlando con {} {}, il suo nickname è {} e il suo ID è {}.'.format(user['first_name'], user['last_name'], user['username'], user['id']))
+    bot.sendMessage(chat_id=update.message.chat_id, text='Il chat ID è: ' + str(update.message.chat_id))
+
+
+chatID_handler = CommandHandler('chatID', chatID)
 dispatcher.add_handler(chatID_handler)
 
-updater.start_polling()
+updater.start_polling(clean=True)
 
 # Per terminare il bot: updater.stop()
+# Per rimuovere handler: dispatcher.remove_handler(echo_handler)
