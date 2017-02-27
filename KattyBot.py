ad7609b9be4d8494645cc04362045cf0254d499d
bruniii <marbrunero@gmail.com>
2017-02-27 23:53:53 +0100

    DB immagini online e prova immagini locali
    Modificata la funzione Batman in modo che scelga casualmente un url dal
    file txt. Compresa la didascalia associata all'immagine.
    
    Aggiunta funzione di prova per mandare una immagine locale.
    
    Modificato il controllo dell'ID chat in modo che possa avere più di un
    valore valido.
    
    Temporaneamente disabilitata la funzione di eco.

--- KattyBot.py
+++ KattyBot.py
@@ -8,6 +8,7 @@
 """
 
 import logging
+import random
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
@@ -15,7 +16,7 @@ from telegram.ext import BaseFilter
 
 flag_Chat = True
 IDchat = 0
-SpamID = 163078549  # Marco: 163078549
+SpamID = {-2861403, 163078549}  # Netflix: -2861403  Marco: 163078549
 
 updater = Updater(token='362591666:AAEwquW77vwbnwDhK2899SGUoW4emmKoLQk')
 
@@ -27,7 +28,7 @@ class FilterSpam(BaseFilter):
     """Filtro da usare per limitare ad un gruppo l'uso del bot."""
     def filter(self, message):
         """Controlla che l'ID della chat sia uguale a quello dello Spam."""
-        return SpamID == message.chat_id
+        return message.chat_id in SpamID
 
 
 class FilterOnOff(BaseFilter):
@@ -60,36 +61,6 @@ stop_handler = CommandHandler('stop', stop)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(stop_handler)
 
-
-""" ----------- COMANDO BATMAN --------------"""
-def batman(bot, update):
-    """La funzione callback che viene chiamata dalla parola chiave batman."""
-    bot.sendPhoto(chat_id=update.message.chat_id, photo='https://selfie.legobatman.com/assets/img/BatmanLegalScreen.png', caption='NANANANA')
-
-
-class FilterBatman(BaseFilter):
-    """Questa parte si occupa di verificare la presenza della parola chiave "batman" nel testo del messaggio. Se è presente, ritorna 1 e quindi viene chiamata la funzione di callback batman."""
-    def filter(self, message):
-        """Ritorna True se la parola batman è presente nel testo del messaggio."""
-        return 'batman' in message.text
-
-
-filter_batman = FilterBatman()
-
-batman_handler = MessageHandler(filter_batman & filter_spam & filter_OnOff, batman)
-dispatcher.add_handler(batman_handler, group=0)
-
-
-""" ----------- COMANDO ECO --------------"""
-def echo(bot, update):
-    """Semplice comando: ogni volta che sniffa un messaggio, risponde con una frase."""
-    bot.sendMessage(chat_id=update.message.chat_id, text='Ogni volta che scrivi qualcosa io rispondo così.')
-
-
-echo_handler = MessageHandler(Filters.text & filter_spam & filter_OnOff, echo)
-dispatcher.add_handler(echo_handler, group=0)
-
-
 """ ----------- COMANDO CHAT ID --------------"""
 def chatID(bot, update):
     """Questo è handler vuoto che uso solo per capire l'ID della chat,
@@ -103,6 +74,63 @@ def chatID(bot, update):
 
 chatID_handler = CommandHandler('chatID', chatID)
 dispatcher.add_handler(chatID_handler)
+
+
+""" ----------- COMANDO BATMAN --------------"""
+def batman(bot, update):
+    """La funzione callback che viene chiamata dalla parola chiave batman."""
+    batmanDB = open('batmanDB.txt', 'r')
+    linee = 0
+    for line in batmanDB:
+        linee = linee + 1
+    batmanDB.seek(0)    # Per tornare ad inizio file.
+
+    n_linea = random.randint(0, linee - 1)
+    estratto = batmanDB.readlines()[n_linea].split(',')
+    link = estratto[0]
+    didascalia = estratto[1]
+    batmanDB.close()
+
+    bot.sendPhoto(chat_id=update.message.chat_id, photo=link, caption=didascalia)
+
+
+class FilterBatman(BaseFilter):
+    """Questa parte si occupa di verificare la presenza della parola chiave "batman" nel testo del messaggio. Se è presente, ritorna 1 e quindi viene chiamata la funzione di callback batman."""
+    def filter(self, message):
+        """Ritorna True se la parola batman è presente nel testo del messaggio."""
+        return 'batman' in message.text.lower()
+
+
+filter_batman = FilterBatman()
+
+batman_handler = MessageHandler(filter_batman & filter_spam & filter_OnOff, batman)
+dispatcher.add_handler(batman_handler, group=0)
+
+""" ----------- COMANDO FILE LOCALE --------------"""
+def foto(bot, update):
+    """La funzione callback che viene chiamata dalla parola chiave batman."""
+    bot.sendPhoto(update.message.chat_id, open('prova.png', 'rb'))
+
+class FilterFoto(BaseFilter):
+    """Questa parte si occupa di verificare la presenza della parola chiave "batman" nel testo del messaggio. Se è presente, ritorna 1 e quindi viene chiamata la funzione di callback batman."""
+    def filter(self, message):
+        """Ritorna True se la parola batman è presente nel testo del messaggio."""
+        return 'foto locale' in message.text.lower()
+
+
+filter_foto = FilterFoto()
+
+foto_handler = MessageHandler(filter_foto & filter_spam & filter_OnOff, foto)
+dispatcher.add_handler(foto_handler, group=0)
+
+""" ----------- COMANDO ECO --------------"""
+def echo(bot, update):
+    """Semplice comando: ogni volta che sniffa un messaggio, risponde con una frase."""
+    bot.sendMessage(chat_id=update.message.chat_id, text='Ogni volta che scrivi qualcosa io rispondo così.')
+
+
+echo_handler = MessageHandler(Filters.text & filter_spam & filter_OnOff, echo)
+# dispatcher.add_handler(echo_handler, group=0)
 
 
 updater.start_polling(clean=True)
